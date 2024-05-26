21aeb2e5ebba258fe54a285b916c0431f96c4cc4
KatTihanovich <128325686+KatTihanovich@users.noreply.github.com>
2024-05-26 18:38:59 +0300

    Update 03_Querying_Partitions_Katsiaryna_Tsikhanovich.sql

--- 03_Querying_Partitions_Katsiaryna_Tsikhanovich.sql
+++ 03_Querying_Partitions_Katsiaryna_Tsikhanovich.sql
@@ -1,7 +1,9 @@
+--Retrieve all sales in a specific month
 SELECT *
 FROM sales_data
 WHERE sale_date BETWEEN '2024-03-01' AND '2024-03-31';
 
+-- Calculate the total sale_amount for each month
 SELECT 
     DATE_TRUNC('month', sale_date) AS month,
     SUM(sale_amount) AS total_sales
@@ -11,7 +13,8 @@ GROUP BY
     month
 ORDER BY 
     month;
-	
+
+--Identify the top three salesperson_id values by sale_amount within a specific region across all partitions.
 SELECT 
     salesperson_id,
     SUM(sale_amount) AS total_sales
